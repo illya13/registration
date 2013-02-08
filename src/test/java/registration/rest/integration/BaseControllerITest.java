3d3c7c19855c4e160c2e427379da41875bfd7349
Illya Havsiyevych <illya_havsiyevych@epam.com>
2013-02-08 09:24:52 +0200

    more integration tests

--- src/test/java/registration/rest/integration/BaseControllerITest.java
+++ src/test/java/registration/rest/integration/BaseControllerITest.java
@@ -6,20 +6,38 @@ import org.eclipse.jetty.server.bio.SocketConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.springframework.web.client.RestTemplate;
+import registration.domain.User;
 
 public class BaseControllerITest {
     private static int port = 7900;
     private static String contextPath = "/registration";
     private static Server server;
 
+    protected static User expectedUser;
+    protected static String expectedToken;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
+        User.Builder builder = new User.Builder();
+        expectedUser = builder.newUser("root").setPassword("qwerty").
+                setFirstName("FirstName").setLastName("LastName").
+                build();
+
+
         startJetty();
+
+        RestTemplate restTemplate = new RestTemplate();
+        expectedToken = restTemplate.getForObject(getRestEndpoint("user", "login", "?id={id}&password={password}"),
+                String.class, expectedUser.getId(), expectedUser.getPassword());
     }
 
     @AfterClass
     public static void afterClass() throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForEntity(getRestEndpoint("user", "logout", "?token={token}"),
+                String.class, expectedToken);
+
         stopJetty();
     }
 
@@ -49,5 +67,9 @@ public class BaseControllerITest {
 
     protected static String getRestEndpoint(String path, String param) {
         return String.format("http://localhost:%1$d%2$s/rest/%3$s/%4$s", port, contextPath, path, param);
+    }
+
+    protected static String getRestEndpoint(String path, String pathParam, String requestParam) {
+        return String.format("http://localhost:%1$d%2$s/rest/%3$s/%4$s%5$s", port, contextPath, path, pathParam, requestParam);
     }
 }
--- src/test/java/registration/rest/integration/UserControllerITest.java
+++ src/test/java/registration/rest/integration/UserControllerITest.java
@@ -6,7 +6,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
+import registration.domain.User;
 
 @ContextConfiguration(locations = {"classpath:spring/rest-client-context.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -17,5 +19,40 @@ public class UserControllerITest extends BaseControllerITest {
     @Test()
     public void testGetRestEndpoint() throws Exception {
         Assert.assertEquals("http://localhost:7900/registration/rest/user/root", getRestEndpoint("user", "root"));
+    }
+
+    @Test(expected = HttpServerErrorException.class)
+    public void testCreateUser() throws Exception {
+        User response = restTemplate.postForObject(getRestEndpoint("user"), expectedUser, User.class);
+        Assert.assertNotNull(response);
+        Assert.assertEquals(expectedUser.getId(), response.getId());
+        Assert.assertEquals(expectedUser.getFirstName(), response.getFirstName());
+        Assert.assertEquals(expectedUser.getLastName(), response.getLastName());
+    }
+
+    @Test()
+    public void testGetUser() throws Exception {
+        User response = restTemplate.getForObject(getRestEndpoint("user", expectedUser.getId(), "?token={token}"),
+                User.class, expectedToken);
+        Assert.assertNotNull(response);
+        Assert.assertEquals(expectedUser.getId(), response.getId());
+        Assert.assertNotNull(response.getFirstName());
+        Assert.assertNotNull(response.getLastName());
+    }
+
+    @Test()
+    public void testGetUsers() throws Exception {
+        User[] response = restTemplate.getForObject(getRestEndpoint("user")+"?token={token}",
+                User[].class, expectedToken);
+        Assert.assertNotNull(response);
+        Assert.assertEquals(1, response.length);
+        Assert.assertEquals(expectedUser.getId(), response[0].getId());
+        Assert.assertNotNull(response[0].getFirstName());
+        Assert.assertNotNull(response[0].getLastName());
+    }
+
+    @Test()
+    public void testDeleteUser() throws Exception {
+        restTemplate.delete(getRestEndpoint("user", expectedUser.getId(), "?token={token}"), expectedToken);
     }
 }
